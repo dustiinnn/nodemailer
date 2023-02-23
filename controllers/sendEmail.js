0c884de4ebde7dda000c72a60363915964353596
Dustin Joe <dustinnjoe@Dustins-MacBook-Pro.local>
2023-02-23 22:05:58 +0700

    nodemailer

--- controllers/sendEmail.js
+++ controllers/sendEmail.js
@@ -4,13 +4,13 @@ const sendEmail = async (req, res) => {
   const transporter = await nodemailer.createTransport({
     service: "outlook",
     auth: {
-      user: "dustinnjoe23@outlook.com",
-      pass: "?P8tsaJxY*A7*6=",
+      user: "testing@outlook.com",
+      pass: "yourOwnPass",
     },
   });
 
   const options = {
-    from: "dustinnjoe23@outlook.com",
+    from: "testing@outlook.com",
     to: ["name@gmail.com"],
     subject: "Sending email with node.js",
     text: "email send success!",
@@ -26,4 +26,3 @@ const sendEmail = async (req, res) => {
 };
 
 module.exports = sendEmail;
-//stelah register sukses, auto kirim email thanks for register!
